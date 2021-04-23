bdc2c25386716ab4de43e01d60ba8009ab65e548
Bryan Alejandro Cubillos Prieto <bryan.cubillos@bizagi.com>
2021-04-22 23:14:11 -0500

    Consume the package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MyLibOneService } from 'my-lib-one';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Welcome Rockstar!!! ';
+
+  constructor(private myLibOneService: MyLibOneService) {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MyLibOneComponent, MyLibOneModule } from 'my-lib-one';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MyLibOneModule
   ],
   providers: [],
   bootstrap: [AppComponent]
